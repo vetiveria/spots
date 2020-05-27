d2a30c60a540a2a04ceda40831e18c3c657a35e8
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2020-05-27 16:45:05 +0100

    Update src/main.py: in progress

--- src/main.py
+++ src/main.py
@@ -1,14 +1,19 @@
 import src.algorithms.tri.settings
 import src.algorithms.tri.api
+import src.boundaries.boundaries
 
 
 def main():
 
     settings = src.algorithms.tri.settings.Settings()
     api = src.algorithms.tri.api.API()
+    entity = src.boundaries.boundaries.Boundaries(crs=settings.crs)
 
     print(settings.crs)
     print(api.url())
+
+    states = entity.states(year=2018)
+    print(states.head())
 
 
 if __name__ == '__main__':
