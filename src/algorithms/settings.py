8f49b320ce35d22d311f23841be11e286cf44804
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2020-05-29 17:34:22 +0100

    Add src/algorithms/settings.py

---
+++ src/algorithms/settings.py
@@ -0,0 +1,10 @@
+
+class Settings:
+
+    def __init__(self):
+
+        # The coordinate reference system
+        self.crs = 4269
+
+        # For geographic boundaries
+        self.latest = 2018
