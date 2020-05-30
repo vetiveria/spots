af8cdf8870049f6d7fa1254718167578155d658f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2020-05-30 12:29:50 +0100

    Update decimal geodetic formula in geodetic.py
    Modifies: [src/functions/geodetic.py]

--- src/functions/geodetic.py
+++ src/functions/geodetic.py
@@ -10,17 +10,23 @@ class Geodetic:
         self.decimal = decimal
         self.decimal.getcontext().prec = 6
 
-    def calculate(self, degrees: int, minutes: int, seconds: int):
+    def getdecimal(self, degrees: int, minutes: int, seconds: int, direction: int):
         dec = self.decimal.Decimal(degrees + (minutes * 60 + seconds) / 3600)
         dec = dec.quantize(self.decimal.Decimal(self.precision), rounding=self.rounding)
 
-        return float(dec)
+        return direction*float(dec)
 
-    def geodetic(self, measure: float):
+    def geodetic(self, measure: float, direction: int = 1):
+        """
+
+        :param measure: The geodetic value whose decimal form is being calculated
+        :param direction: The longitude sign -> +1 eastward, -1 westward
+        :return:
+        """
         arc = str(int(measure)).zfill(6)
 
         degrees = int(arc[:2])
         minutes = int(arc[2:4])
         seconds = int(arc[4:])
 
-        return self.calculate(degrees=degrees, minutes=minutes, seconds=seconds)
+        return self.getdecimal(degrees=degrees, minutes=minutes, seconds=seconds, direction=direction)
