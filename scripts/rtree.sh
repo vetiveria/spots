cb0b7de3ea03b99920482afde5232b5b004a7a90
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2020-07-25 18:28:24 +0100

    Update scripts/rtree.sh

--- scripts/rtree.sh
+++ scripts/rtree.sh
@@ -6,5 +6,5 @@ package=Rtree-0.9.4
 # Unpack
 curl -L https://files.pythonhosted.org/packages/56/6f/f1e91001d5ad9fa9bed65875152f5a1c7955c5763168cae309546e6e9fda/${package}.tar.gz | tar xz
 
-# Set-up
+# Setting-up
 cd ${package} && /usr/local/bin/python setup.py install
